071292ce0a87265569189ee00bba6c9a713d8d8c
unknown <atefislam61@gmail.com>
2024-01-01 19:44:11 +0100

    re_organize

---
+++ Assignments/Assignment_3_ARR&STR/Av_Calc.c
@@ -0,0 +1,40 @@
+/*
+ ============================================================================
+ Name        : Av_Calc.c
+ Author      : ISLAM_ATEF
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Av_Calc in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n , i ,sum =0  ;
+
+	printf ("please enter the number of element\n");
+
+	fflush(stdout);
+
+	scanf("%d",&n);
+
+	int arr[n] ;
+
+
+	for (i= 0 ; i < n ; i ++)
+	{
+		printf("E%d",i+1 );
+		fflush(stdout);
+
+		scanf("%d",&arr[i]);
+		sum += arr[i];
+
+	}
+	printf ("the average is %d",sum/n);
+
+
+	return 0;
+}
